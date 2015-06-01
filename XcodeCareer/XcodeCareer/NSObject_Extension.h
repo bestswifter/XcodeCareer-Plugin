1e99a247e62630ffefa756bb5d7a6ae65e16ddf6
KT <KT@IP-58-154-203-172.neu.edu.cn>
2015-06-01 08:50:33 +0800

    Initial commit

---
+++ XcodeCareer/XcodeCareer/NSObject_Extension.h
@@ -0,0 +1,16 @@
+//
+//  NSObject_Extension.h
+//  XcodeCareer
+//
+//  Created by KT on 6/1/15.
+//  Copyright (c) 2015 KT. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSObject (Xcode_Plugin_Template_Extension)
+
++ (void)pluginDidLoad:(NSBundle *)plugin;
+
+
+@end
---
+++ XcodeCareer/XcodeCareer/XcodeCareer.m
@@ -0,0 +1,66 @@
+//
+//  XcodeCareer.m
+//  XcodeCareer
+//
+//  Created by KT on 6/1/15.
+//  Copyright (c) 2015 KT. All rights reserved.
+//
+
+#import "XcodeCareer.h"
+
+@interface XcodeCareer()
+
+@property (nonatomic, strong, readwrite) NSBundle *bundle;
+@end
+
+@implementation XcodeCareer
+
++ (instancetype)sharedPlugin
+{
+    return sharedPlugin;
+}
+
+- (id)initWithBundle:(NSBundle *)plugin
+{
+    if (self = [super init]) {
+        // reference to plugin's bundle, for resource access
+        self.bundle = plugin;
+        [[NSNotificationCenter defaultCenter] addObserver:self
+                                                 selector:@selector(didApplicationFinishLaunchingNotification:)
+                                                     name:NSApplicationDidFinishLaunchingNotification
+                                                   object:nil];
+    }
+    return self;
+}
+
+- (void)didApplicationFinishLaunchingNotification:(NSNotification*)noti
+{
+    //removeObserver
+    [[NSNotificationCenter defaultCenter] removeObserver:self name:NSApplicationDidFinishLaunchingNotification object:nil];
+    
+    // Create menu items, initialize UI, etc.
+    // Sample Menu Item:
+    NSMenuItem *menuItem = [[NSApp mainMenu] itemWithTitle:@"Edit"];
+    if (menuItem) {
+        [[menuItem submenu] addItem:[NSMenuItem separatorItem]];
+        NSMenuItem *actionMenuItem = [[NSMenuItem alloc] initWithTitle:@"Do Action" action:@selector(doMenuAction) keyEquivalent:@""];
+        //[actionMenuItem setKeyEquivalentModifierMask:NSAlphaShiftKeyMask | NSControlKeyMask];
+        [actionMenuItem setTarget:self];
+        [[menuItem submenu] addItem:actionMenuItem];
+    }
+}
+
+// Sample Action, for menu item:
+- (void)doMenuAction
+{
+    NSAlert *alert = [[NSAlert alloc] init];
+    [alert setMessageText:@"Hello, World"];
+    [alert runModal];
+}
+
+- (void)dealloc
+{
+    [[NSNotificationCenter defaultCenter] removeObserver:self];
+}
+
+@end
